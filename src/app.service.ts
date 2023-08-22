d898d8dcf7121bdab04aa33e50648f7f0ee93592
Rodrigo <diggo03@hotmail.com>
2023-08-22 08:10:12 -0300

    ajustes formatação

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return "Serviço iniciado na porta 3000";
   }
 }
